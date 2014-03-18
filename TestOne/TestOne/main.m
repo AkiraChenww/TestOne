bcddaf1a55839249e99c1b327c0effdac508b4dd
lustyangel <631678824@qq.com>
2014-03-18 16:50:13 +0800

    123
    231

---
+++ TestOne/TestOne/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestOne
+//
+//  Created by TY on 14-3-18.
+//  Copyright (c) 2014年 Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
